f2e8a108858bd6f552d4e0275cc694ab52cfafc4
Tobias Schwab <tobias.schwab@dynport.de>
2012-03-02 19:35:14 +0100

    Initial commit

---
+++ lib/growl-ampelmaennchen.rb
@@ -0,0 +1,7 @@
+require "growl-ampelmaennchen/version"
+
+module Growl
+  module Ampelmaennchen
+    # Your code goes here...
+  end
+end
---
+++ lib/growl-ampelmaennchen/version.rb
@@ -0,0 +1,5 @@
+module Growl
+  module Ampelmaennchen
+    VERSION = "0.0.1"
+  end
+end
